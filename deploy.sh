8b848563050289f93c6d5aeffb93a67769bcc603
vadim <filinvadim@pm.me>
2024-12-31 00:09:30 +0400

    Add deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Run deploy script"
-echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
-echo "nameserver 1.1.1.1" | sudo tee -a /etc/resolv.conf
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 1.1.1.1" >> /etc/resolv.conf
 docker pull ghcr.io/filinvadim/warpnet:latest
 docker run -d --name warpnet -p 4001:4001 -p 4002:4002 ghcr.io/filinvadim/warpnet:latest
