b29ab73ab944dce66779785ae33a57f852fdf78f
vadim <filinvadim@protonmail.com>
2025-02-01 12:43:19 +0400

    WIP

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,8 @@ if [ -z "$GITHUB_TOKEN" ]; then
   echo "Error: GITHUB_TOKEN is not set"
   exit 1
 fi
-docker system prune -f
+docker stop $(docker ps -a -q)
+docker system prune -f -a
 echo $GITHUB_TOKEN | docker login ghcr.io -u filinvadim --password-stdin
 docker pull ghcr.io/filinvadim/warpnet:latest
 docker run --restart=always -d --name warpnet -p 4001:4001 -p 4002:4002 ghcr.io/filinvadim/warpnet:latest
