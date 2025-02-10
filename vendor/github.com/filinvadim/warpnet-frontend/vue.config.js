fee3653c296161df33d40035e102a761fc42ad55
vadim <filinvadim@protonmail.com>
2025-02-10 12:52:01 +0400

    WIP

--- vendor/github.com/filinvadim/warpnet-frontend/vue.config.js
+++ vendor/github.com/filinvadim/warpnet-frontend/vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
         resolve: {
             fallback: {
                 buffer: require.resolve('buffer'),
+                "fs": false,
             },
         },
         plugins: [
