474641782fbb4379eef00b74d934da11602ae6a7
vadim <filinvadim@protonmail.com>
2025-02-21 23:46:16 +0400

    WIP

--- config/config.go
+++ config/config.go
@@ -10,9 +10,9 @@ import (
 )
 
 var defaultBootstrapNodes = []string{
-	"/ip4/67.207.72.168/tcp/4001/p2p/12D3KooWMKZFrp1BDKg9amtkv5zWnLhuUXN32nhqMvbtMdV2hz7j",
-	"/ip4/67.207.72.168/tcp/4002/p2p/12D3KooWSjbYrsVoXzJcEtmgJLMVCbPXMzJmNN1JkEZB9LJ2rnmU",
-	"/ip4/67.207.72.168/tcp/4003/p2p/12D3KooWNXSGyfTuYc3JznW48jay73BtQgHszWfPpyF581EWcpGJ",
+	"/ip4/207.154.221.44/tcp/4001/p2p/12D3KooWMKZFrp1BDKg9amtkv5zWnLhuUXN32nhqMvbtMdV2hz7j",
+	"/ip4/207.154.221.448/tcp/4002/p2p/12D3KooWSjbYrsVoXzJcEtmgJLMVCbPXMzJmNN1JkEZB9LJ2rnmU",
+	"/ip4/207.154.221.44/tcp/4003/p2p/12D3KooWNXSGyfTuYc3JznW48jay73BtQgHszWfPpyF581EWcpGJ",
 }
 
 var ConfigFile Config
