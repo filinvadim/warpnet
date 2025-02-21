b3b2dc13e281aa6821b8a6ee2c26e13cb600edc9
vadim <filinvadim@protonmail.com>
2025-02-21 17:30:16 +0400

    FIX

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,8 @@ echo $GITHUB_TOKEN | docker login ghcr.io -u filinvadim --password-stdin
 docker pull ghcr.io/filinvadim/warpnet:latest
 iptables -I DOCKER-USER -j ACCEPT
 iptables -A INPUT -p tcp --match multiport --dports 4001:4003 -j ACCEPT
+iptables -A FORWARD -i docker0 -o docker0 -j ACCEPT
+iptables -A FORWARD -i br-6383b19e4979 -o br-6383b19e4979 -j ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 docker compose up -d
