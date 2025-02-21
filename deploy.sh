b33533e81f7a6bbac8625e5b74e164055c48c4fa
vadim <filinvadim@protonmail.com>
2025-02-21 23:31:56 +0400

    WIP

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,7 @@ docker compose down
 echo $GITHUB_TOKEN | docker login ghcr.io -u filinvadim --password-stdin
 docker pull ghcr.io/filinvadim/warpnet:latest
 ufw disable
+systemctl restart systemd-networkd
 iptables -I DOCKER-USER -j ACCEPT
 iptables -A INPUT -p tcp --match multiport --dports 4001:4003 -j ACCEPT
 iptables -A FORWARD -i docker0 -o docker0 -j ACCEPT
